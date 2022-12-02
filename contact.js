a910e002bf94ef24dc5c6eaaa5b4db5923806698
AlfaNashiha26 <103365359+AlfaNashiha26@users.noreply.github.com>
2022-12-02 18:13:27 +0700

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,14 @@
+function chat() {
+    const message = "hai min.. saya mau tanya tentang produk nya"
+    const whatsapp = "https://wa.me/+6285323147012"
+    open(whatsapp, "_blank")
+}
+
+function redirinstagram() {
+    window.location.href = "https://instagram.com/alfa.nashiha_"
+}
+
+function redirmailto() {
+    window.location.href = "mailto:alfanashiha79@gmail.com"
+}
+
